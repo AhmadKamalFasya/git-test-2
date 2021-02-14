927b825c8621c722db174d70808ab70d1d1d3501
Ahmad Kamal Fasya <fasyaaoi9@gmail.com>
2021-02-14 10:41:42 +0700

    Menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Git test 2</title>
+</head>
+
+<body>
+    <h4>Hello World !</h4>
+</body>
+
+</html>
